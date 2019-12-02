aff556deca5dfc3651d4a60f9dc7bb3d68dfe6ed
Uwe <uwe42@gmx.de>
2019-12-02 22:31:56 +0100

    initial commit

---
+++ src/api.js
@@ -0,0 +1,26 @@
+const express = require('express');
+const serverless = require('serverless-http');
+const app = express();
+
+const router = express.Router();
+
+router.get('/', (req, res) => {
+    res.json({
+        'class':'hi!'
+    });
+});
+router.get('/valeska', (req, res) => {
+    res.json({
+        'name':'Valeska',
+        'class':'Knight',
+        'spec':'Archers',
+        'skill1':'Basic Leadership',
+        'skill2':'Basic Archery',
+        'spell':'-'
+    });
+});
+
+app.use('/.netlify/functions/api/', router);
+
+module.exports.handler = serverless(app);
+
